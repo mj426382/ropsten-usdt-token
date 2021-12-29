e72f957f8083fd7dd9c72e9963a09286963590f0
mateusz <mistrzumati@spoko.pl>
2021-12-29 14:55:07 +0100

    Finish

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  UsdtToken,
   Issue,
   Redeem,
   Deprecate,
@@ -13,66 +11,12 @@ import {
   Pause,
   Unpause
 } from "../generated/UsdtToken/UsdtToken"
-import { ExampleEntity } from "../generated/schema"
+import { TransferEvent } from "../generated/schema"
 
-export function handleIssue(event: Issue): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+const lender = '0x16d02Dc67EB237C387023339356b25d1D54b0922'
+const lender2 = '0xa606dd423dF7dFb65Efe14ab66f5fDEBf62FF583'
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.amount = event.params.amount
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.name(...)
-  // - contract.deprecated(...)
-  // - contract.totalSupply(...)
-  // - contract.upgradedAddress(...)
-  // - contract.balances(...)
-  // - contract.decimals(...)
-  // - contract.maximumFee(...)
-  // - contract._totalSupply(...)
-  // - contract.getBlackListStatus(...)
-  // - contract.allowed(...)
-  // - contract.paused(...)
-  // - contract.balanceOf(...)
-  // - contract.getOwner(...)
-  // - contract.owner(...)
-  // - contract.symbol(...)
-  // - contract.allowance(...)
-  // - contract.basisPointsRate(...)
-  // - contract.isBlackListed(...)
-  // - contract.MAX_UINT(...)
-}
+export function handleIssue(event: Issue): void {}
 
 export function handleRedeem(event: Redeem): void {}
 
@@ -88,7 +32,21 @@ export function handleRemovedBlackList(event: RemovedBlackList): void {}
 
 export function handleApproval(event: Approval): void {}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+
+  if(event.params.from.toHexString() === lender || event.params.from.toHexString() === lender2) {
+    return
+  }
+
+  const entity = new TransferEvent(event.transaction.hash.toHex())
+
+  entity.value = event.params.value
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.blockNumber = event.block.number
+
+  entity.save()
+}
 
 export function handlePause(event: Pause): void {}
 
